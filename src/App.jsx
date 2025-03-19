5b18a24e6a05fa5a250ec43066c9123685565d14
bradley-velle <bradley.velle@student.howest.be>
2025-03-19 18:00:26 +0100

    Fix vercel not building project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { AuthProvider } from "./context/AuthContext.jsx"; // Import the AuthProvider
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
-import Login from "./pages/Login/Login.jsx";
-import Register from "./pages/Register/Register.jsx";
+import Login from "./pages/login/Login.jsx";
+import Register from "./pages/register/Register.jsx";
 import Home from "./pages/Home";
 
 export default function App() {
