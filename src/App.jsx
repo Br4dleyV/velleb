14eb2dae4012e76f83f5e6d1fe0efd7a3ab045ed
bradley-velle <bradley.velle@student.howest.be>
2025-03-19 16:43:20 +0100

    Sort CSS files and fix some F12 inspect issues from 'issues' tab

--- src/App.jsx
+++ src/App.jsx
@@ -63,61 +63,63 @@ export default function App() {
   return <>
     <header>
       <nav>
-        <li>
-          {/* Hamburger Button & Logo */}
-          <button type="button" onClick={() => setMenuOpen(!menuOpen)}>
-            <motion.svg key={menuOpen ? "close" : "menu"} initial={{ opacity: 0, rotate: -90, scale: 0.8 }} animate={{ opacity: 1, rotate: 0, scale: 1 }}>
-              {menuOpen ? (<motion.path d="M6 18L18 6M6 6l12 12" />) : (<motion.path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />)}
-            </motion.svg>
-          </button>
-          <img src="/BV.png" alt="BradleyV" />
-        </li>
+        <ul>
+          <li>
+            {/* Hamburger Button & Logo */}
+            <button type="button" title="Hamburger Button" onClick={() => setMenuOpen(!menuOpen)}>
+              <motion.svg key={menuOpen ? "close" : "menu"} initial={{ opacity: 0, rotate: -90, scale: 0.8 }} animate={{ opacity: 1, rotate: 0, scale: 1 }}>
+                {menuOpen ? (<motion.path d="M6 18L18 6M6 6l12 12" />) : (<motion.path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />)}
+              </motion.svg>
+            </button>
+            <img src="/BV.png" alt="BradleyV" />
+          </li>
 
-        {/* Normal Nav Bar */}
-        <li>
-          <a href="#">Dashboard</a>
-          <a href="#">Team</a>
-          <a href="#">Projects</a>
-          <a href="#">Calendar</a>
-        </li>
+          {/* Normal Nav Bar */}
+          <li>
+            <a href="#">Dashboard</a>
+            <a href="#">Team</a>
+            <a href="#">Projects</a>
+            <a href="#">Calendar</a>
+          </li>
 
-        {/* Profile Dropdown / Login-Register */}
-        <li>
-          {user ? (
-            <>
-              <button onClick={() => setProfileOpen(!profileOpen)} type="button" ref={profileMenuRef}>
-                <img src={profilePictureUrl} alt="Profile Image" />
-                {profileOpen && (
-                  <motion.div initial={{ opacity: 0, translateY: -10 }} animate={{ opacity: 1, translateY: 0 }} ref={profileDropdownRef}>
-                    <a href="#">Your Profile</a>
-                    <a href="#">Settings</a>
-                    <a href="#" onClick={handleLogout}>Sign out</a>
-                  </motion.div>
-                )}
-              </button>
-            </>
-          ) : (
-            <>
-              <a className="button button-green" href="/login">Login</a>
-              <a className="button button-white" href="/register">Register</a>
-            </>
+          {/* Profile Dropdown / Login-Register */}
+          <li>
+            {user ? (
+              <>
+                <button onClick={() => setProfileOpen(!profileOpen)} type="button" ref={profileMenuRef}>
+                  <img src={profilePictureUrl} alt="Profile Image" />
+                  {profileOpen && (
+                    <motion.div initial={{ opacity: 0, translateY: -10 }} animate={{ opacity: 1, translateY: 0 }} ref={profileDropdownRef}>
+                      <a href="#">Your Profile</a>
+                      <a href="#">Settings</a>
+                      <a href="#" onClick={handleLogout}>Sign out</a>
+                    </motion.div>
+                  )}
+                </button>
+              </>
+            ) : (
+              <>
+                <a className="button button-green" href="/login">Login</a>
+                <a className="button button-white" href="/register">Register</a>
+              </>
+            )}
+          </li>
+        </ul>
+        {/* Mobile Nav Bar */}
+        <AnimatePresence>
+          {menuOpen && (
+            <motion.nav initial={{ height: 0, opacity: 0 }} animate={{ height: "auto", opacity: 1 }} exit={{ height: 0, opacity: 0 }} transition={{ duration: 0.3, ease: "easeInOut" }}> {/* Add overflow-hidden to prevent content from being visible during animation*/}
+              <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.15 } }} exit={{ opacity: 0 }}>
+                <a href="#">Dashboard</a>
+                <a href="#">Team</a>
+                <a href="#">Projects</a>
+                <a href="#">Calendar</a>
+              </motion.div>
+            </motion.nav>
           )}
-        </li>
+        </AnimatePresence>
       </nav>
 
-      {/* Mobile Nav Bar */}
-      <AnimatePresence>
-        {menuOpen && (
-          <motion.nav initial={{ height: 0, opacity: 0 }} animate={{ height: "auto", opacity: 1 }} exit={{ height: 0, opacity: 0 }} transition={{ duration: 0.3, ease: "easeInOut" }}> {/* Add overflow-hidden to prevent content from being visible during animation*/}
-            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.15 } }} exit={{ opacity: 0 }}>
-              <a href="#">Dashboard</a>
-              <a href="#">Team</a>
-              <a href="#">Projects</a>
-              <a href="#">Calendar</a>
-            </motion.div>
-          </motion.nav>
-        )}
-      </AnimatePresence>
     </header>
 
     {/* Router */}
