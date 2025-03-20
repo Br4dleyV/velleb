21d85dd900256266f5c3ad996bdd82682db151cb
Velle Bradley <bradley.velle@student.howest.be>
2025-03-20 11:31:41 +0100

    Implement AlertContext in App.jsx #6

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { AlertProvider } from "./context/AlertContext";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home.jsx";
@@ -9,18 +10,20 @@ import UpdatePassword from "./pages/authentication/UpdatePassword.jsx";
 
 export default function App() {
   return <>
-    <AuthProvider>
-      <Header />
+    <AlertProvider>
+      <AuthProvider>
+        <Header />
 
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/update-password" element={<UpdatePassword />} />
-        </Routes>
-      </main>
-    </AuthProvider>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+            <Route path="/update-password" element={<UpdatePassword />} />
+          </Routes>
+        </main>
+      </AuthProvider>
+    </AlertProvider>
   </>;
 }
